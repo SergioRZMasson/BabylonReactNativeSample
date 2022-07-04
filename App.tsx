7905d5a4db80c77cca672a01db9da920ba3dd757
Sergio Ricardo Zerbetto Masson <sergiom@Sergios-MacBook-Air.local>
2022-07-04 16:08:33 -0300

    Added GUI example

--- App.tsx
+++ App.tsx
@@ -14,6 +14,26 @@ import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
 import "@babylonjs/loaders/glTF";
 import { Scene } from "@babylonjs/core/scene";
 import { WebXRSessionManager, WebXRTrackingState } from "@babylonjs/core/XR";
+import {DynamicTexture} from "@babylonjs/core/Materials/Textures/dynamicTexture";
+import {StandardMaterial} from "@babylonjs/core/Materials/standardMaterial"
+import { Mesh } from "@babylonjs/core";
+import {MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder";
+
+function buildUI(scene : Scene, plane: Mesh) : void
+{
+  //Create dynamic texture
+  var textureResolution = 512;
+	var textureGround = new DynamicTexture("dynamic texture", {width:512, height:256}, scene);   
+	var textureContext = textureGround.getContext();
+	
+	var materialGround = new StandardMaterial("Mat", scene);    				
+	materialGround.diffuseTexture = textureGround;
+	plane.material = materialGround;
+	
+  //Add text to dynamic texture
+  var font = "bold 220px monospace";
+  textureGround.drawText("Grass", 75, 135, font, "green", "black", true, true);
+}
 
 const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
   const engine = useEngine();
@@ -63,6 +83,10 @@ const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
         (scene.activeCamera as ArcRotateCamera).alpha += Math.PI;
         (scene.activeCamera as ArcRotateCamera).radius = 10;
         setCamera(scene.activeCamera!);
+        var groundWidth = 20;
+        var groundHeight = 10;
+        var ground = MeshBuilder.CreateGround("ground1", {width: groundWidth, height: groundHeight, subdivisions: 25}, scene);
+        buildUI(scene, ground)
       });
     }
   }, [engine]);
